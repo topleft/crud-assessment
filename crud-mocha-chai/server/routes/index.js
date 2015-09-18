1718c0ce34124d353a1ad5d7c447e9cbf302e625
topleft <pete.james.jeffryes@gmail.com>
2015-09-18 14:54:25 -0600

    crud app with tests

--- crud-mocha-chai/server/routes/index.js
+++ crud-mocha-chai/server/routes/index.js
@@ -13,4 +13,52 @@ router.get('/items', function(req, res, next) {
 	});
 });
 
+router.get('/items/:id', function(req, res, next) {
+	Item.find({_id: req.params.id}, function(err, data){
+		if (err){
+			res.json(err);
+		} else {
+			res.json(data);
+		}
+	});
+});
+
+router.post('/items/:name/:location', function(req, res, next){
+	var newItem = new Item({
+		name: req.params.name, 
+		location: req.params.location});
+	newItem.save(function(err, data){
+			if (err){
+				res.json(err);
+			} else {
+				res.json(data);
+			}	
+	});
+});
+
+router.put('/items/:id/:name/:location', function(req, res, next) {
+		var query = {_id: req.params.id};
+		var update = {name: req.params.name, location: req.params.location};
+		var option = {new: true};
+	Item.findOneAndUpdate(query, update, option, function(err, data){
+		if (err){
+			res.json(err);
+		} else {
+			res.json(data);
+		}
+	});
+});
+
+router.delete('/items/:id', function(req, res, next) {
+	Item.remove({_id: req.params.id}, function(err, data){
+		if (err){
+			res.json(err);
+		} else {
+			res.json(data);
+		}
+	});
+});
+
+
+
 module.exports = router;
--- crud-mocha-chai/test/test-crud.js
+++ crud-mocha-chai/test/test-crud.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 
 describe("Items", function() {
 	Item.collection.drop();
+	var id;
 
 
 	beforeEach(function(done){
@@ -17,6 +18,7 @@ describe("Items", function() {
 			name: "mop",
 			location: "closet"
 		});
+		id = newItem._id;
 		newItem.save(function(err){
 			done();
 		});
@@ -42,15 +44,70 @@ describe("Items", function() {
 					done();
 				});
 		});
-  it('should list a SINGLE blob on /blob/<id> GET');
-  it('should add a SINGLE blob on /blobs POST'); it('should update a SINGLE blob on /blob/<id> PUT');
-  it('should delete a SINGLE blob on /blob/<id> DELETE');
+  it('should list a SINGLE blob on /blob/<id> GET',
+  	function(done){
+	  		chai.request(server)	
+	  			.get('/items/'+ id)
+	  			.end(function(err, res){
+	  				res.should.have.status(200);
+	  				res.should.be.json;
+	  				res.body.should.be.a('array');
+	  				res.body[0].should.be.a('object');
+	  				res.body[0].name.should.be.a('string');
+						res.body[0].name.should.equal('mop');
+						res.body[0].location.should.be.a('string');
+						res.body[0].location.should.equal('closet');
+						done();
+	  			});
+  			});
+  it('should add a SINGLE blob on /blobs POST',
+  	function(done){
+  		chai.request(server)
+  		.post('/items/chair/livingroom')
+  		.end(function(err, res){
+  			res.should.have.status(200);
+  			res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.name.should.be.a('string');
+				res.body.name.should.equal('chair');
+				res.body.location.should.be.a('string');
+				res.body.location.should.equal('livingroom');
+				done();
+  		});
+  	}); 
 
+
+  it('should update a SINGLE blob on /blob/<id> PUT',
+  	function(done){
+	  		chai.request(server)	
+	  			.put('/items/'+ id +'/broom/closet')
+	  			.end(function(err, res){
+	  				res.should.have.status(200);
+	  				res.should.be.json;
+	  				res.body.should.be.a('object');
+	  				res.body.name.should.be.a('string');
+						res.body.name.should.equal('broom');
+						res.body.location.should.be.a('string');
+						res.body.location.should.equal('closet');
+						done();
+	  			})
+  			});
+
+  it('should delete a SINGLE blob on /blob/<id> DELETE', 
+  	function(done){
+	  		chai.request(server)	
+	  			.delete('/items/'+ id)
+	  			.end(function(err, res){
+	  				console.log(res.body);
+	  				res.should.have.status(200);
+	  				res.should.be.json;
+	  				res.body.should.be.a('object');
+						done();
+  			});
+  		});
 
 
 // closes describe
 });
-
-
 
 
